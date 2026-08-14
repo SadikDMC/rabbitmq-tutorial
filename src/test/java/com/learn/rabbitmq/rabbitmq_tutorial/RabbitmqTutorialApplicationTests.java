52c0ccf88fcef47ea610eae422c14bcd8a5b06b9
Sadik Vajarala <vsadikmca@gmail.com>
2026-08-14 11:56:41 +0530

    Created queue,exchange and binding for rabbitmq

---
+++ src/test/java/com/learn/rabbitmq/rabbitmq_tutorial/RabbitmqTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.rabbitmq.rabbitmq_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
