d5a6bc8cce24c977211e84dbc28a6e80408c9414
Sadik Vajarala <vsadikmca@gmail.com>
2026-08-15 07:36:54 +0530

    added configurations to send and recieve json messages and created a dto class

--- src/main/java/com/learn/rabbitmq/rabbitmq_tutorial/config/RabbitmqConfig.java
+++ src/main/java/com/learn/rabbitmq/rabbitmq_tutorial/config/RabbitmqConfig.java
@@ -1,6 +1,10 @@
 package com.learn.rabbitmq.rabbitmq_tutorial.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,9 +21,21 @@ public class RabbitmqConfig {
     @Value("${rabbitmq.routing.key}")
     private String routingKey;
 
+
+    @Value("${rabbitmq.queue.json.name}")
+    private String jsonQueue;
+
+    @Value("${rabbitmq.routing.json.key}")
+    private String jsonRoutingKey;
+
     @Bean
     public Queue queue(){
         return new Queue(queue);
+    }
+
+    @Bean
+    public Queue jsonQueue(){
+        return new Queue(jsonQueue);
     }
 
     @Bean
@@ -31,4 +47,23 @@ public class RabbitmqConfig {
     public Binding binding() {
         return BindingBuilder.bind(queue()).to(exchange()).with(routingKey);
     }
+
+    @Bean
+    public Binding jsonBinding() {
+        return BindingBuilder.bind(jsonQueue()).to(exchange()).with(jsonRoutingKey);
+    }
+
+    @Bean
+    public MessageConverter converter(){
+        return new JacksonJsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter());
+        return rabbitTemplate;
+    }
+
+
 }
